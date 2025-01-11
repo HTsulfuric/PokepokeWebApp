75a58b38e3f94dc955bc62289bf49617ac028876
HTsulfuric <leo.phoenix2003@gmail.com>
2025-01-12 03:30:07 +0900

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,23 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import { initializeApp } from 'firebase/app'
+
+export const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+};
+
+//@ts-expect-error TS6133 (TS6133) 'app' is declared but its value is never read.
+const app = initializeApp(firebaseConfig); // eslint-disable-line
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
